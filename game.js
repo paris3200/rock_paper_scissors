21547081c594c1d35e9c850aaab24584e0a5eb3f
Jason Paris <paris3200@gmail.com>
2023-10-23 17:16:47 -0400

    Add playRound()
    Adds the ability to determine the winner in a round.

--- game.js
+++ game.js
@@ -5,3 +5,33 @@ function getComputerChoice(){
 
   return CHOICES[randIndex];
 };
+
+function playRound(playerSelection, computerSelection) {
+  let player = playerSelection.toLowerCase()
+  let computer = computerSelection.toLowerCase()
+  let result = ""
+  
+  if (player == "rock" && computer == "paper"){
+    result = "You Lose! Paper beats Rock.";
+
+  } else if (player == "rock" && computer == "scissors"){
+    result = "You Win! Rock beats scissors.";
+
+  } else if (player == computer){
+    result = "You tied!";
+
+  } else if (player == "paper" && computer == "rock"){
+    result = "You Win! Paper beats rock."
+
+  } else if (player == "paper" && computer == "scissors"){
+    result = "You Lose! Scissors beats paper."
+
+  } else if (player == "scissors" && computer == "rock"){
+    result = "You Lose! Rock beats scissors."
+
+  } else if (player == "scissors" && computer == "paper"){
+    result = "You Win! Scissors beats paper."
+  }
+
+  return result;
+};
